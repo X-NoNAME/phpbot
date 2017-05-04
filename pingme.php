98d891d233a64c1e812e6f81888573f11843063b
Ivan Kazakov <i.kazakov@corp.badoo.com>
2017-05-04 10:09:49 +0300

    test

---
+++ pingme.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ikazakov
+ * Date: 04.05.17
+ * Time: 10:07
+ */
+file_get_contents("https://api.telegram.org/bot3116".
+    "26585:AAENuUL1PDtO9YuYxYKYbSWPFzAghgLTa4U/sendMessage?chat_id=71086029&text=".
+    "GET:".json_encode($_GET)."\nPOST:".json_encode($_POST));
