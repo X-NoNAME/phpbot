e2726d6be64e329dc1778bf4d58b9b03f84004c8
x-noname <i.kazakov@corp.badoo.com>
2017-05-05 10:17:10 +0300

    tt

--- pingme.php
+++ pingme.php
@@ -5,5 +5,6 @@
  * Date: 04.05.17
  * Time: 10:07
  */
+$update = file_get_contents('php://input');
 file_get_contents("https://api.telegram.org/bot3116".
-"26585:AAENuUL1PDtO9YuYxYKYbSWPFzAghgLTa4U/sendMessage?chat_id=71086029&text=MSG>".urlencode(json_encode($_REQUEST)));
+"26585:AAENuUL1PDtO9YuYxYKYbSWPFzAghgLTa4U/sendMessage?chat_id=71086029&text=MSG>".urlencode(update));
