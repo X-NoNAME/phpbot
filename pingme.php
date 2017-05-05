072ea0b55a1a97fb003218a8064c7858076cf371
x-noname <i.kazakov@corp.badoo.com>
2017-05-05 10:35:30 +0300

    test

--- pingme.php
+++ pingme.php
@@ -5,7 +5,7 @@
  * Date: 04.05.17
  * Time: 10:07
  */
-$update = json_decode(file_get_contents('php://input'));
+$update = json_decode(file_get_contents('php://input'),true);
 $token = $_ENV["TOKEN"];
 $chat_id=$update['message']["chat"]["id"];
 file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=71086029&text=Hello, chat id is $chat_id");
