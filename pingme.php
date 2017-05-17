6c2b7a9ca861cce7e932e6e1e4240a14124526d9
x-noname <i.kazakov@corp.badoo.com>
2017-05-17 14:04:21 +0300

    test

--- pingme.php
+++ pingme.php
@@ -8,5 +8,7 @@
 $update = json_decode(file_get_contents('php://input'),true);
 $token = $_ENV["TOKEN"];
 $chat_id=$update['message']["chat"]["id"];
+//71086029
+$sig=sizeof($chat_id)+$chat_id[0];
 file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$chat_id."&text="
-    .urlencode("Привет, ваша сылка для получения сообщений: http://tinyurl.com/l3y4x5t?msg=$chat_id:{ТУТ_ВАШЕ_СООБЩЕНИЕ}"));
+    .urlencode("Привет, ваша сылка для получения сообщений: http://tinyurl.com/l3y4x5t?msg=$chat_id"."_"."$sig:{ТУТ_ВАШЕ_СООБЩЕНИЕ}"));
